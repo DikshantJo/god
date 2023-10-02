1445d3c55575e5986a6e3bbd870e4558c4c47d33
DikshantJoshi <85272514+DikshantJo@users.noreply.github.com>
2023-10-02 16:42:19 +0530

    Update product-schema.js

--- sanity/schemas/product-schema.js
+++ sanity/schemas/product-schema.js
@@ -1,44 +1,52 @@
 const Product = {
-    name: "Product",
-    title: "Product Photography",
-    type: "document",
-    fields: [
-      {
-        name: "name",
-        title: "Name",
-        type: "string",
-      },
-      {
-        name: "slug",
-        title: "Slug",
-        type: "slug",
-        options: { source: "name" }
-      },
-      {
-        name: "image",
-        title: "Image",
-        type: "image",
-        options: { hotspot: true },
-        fields: [
-          {
-            name: "alt",
-            title: "Alt",
-            type: "string"
-          }
-        ]
-      },
-      {
-        name: "url",
-        title: "URL",
-        type: "url"
-      },
-      {
-        name: "content",
-        title: "Content",
-        type: "array",
-        of: [{ type: "block" }]
-      }
-    ]
-  }
-  
-  export default Product;
+  name: "Product",
+  title: "Product Photography",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+      validation: (Rule) => Rule.required(), // Use a fat arrow here
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: { source: "name" },
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: { hotspot: true },
+      validation: (Rule) => Rule.required(), // Use a fat arrow here
+      fields: [
+        {
+          name: "alt",
+          title: "Alt",
+          type: "string",
+        },
+      ],
+    },
+    {
+      name: "url",
+      title: "URL",
+      type: "url",
+    },
+    {
+      name: "content",
+      title: "Content",
+      type: "array",
+      of: [{ type: "block" }],
+    },
+    {
+      name: "faqs",
+      title: "FAQs",
+      type: "array",
+      of: [{ type: "faq" }], // Reference the FAQ schema
+    },
+  ],
+};
+
+export default Product;
