e38efeccea45ada2cdc1d75981804850fe924f5f
DikshantJoshi <85272514+DikshantJo@users.noreply.github.com>
2023-10-02 16:42:50 +0530

    Update index.js

--- sanity/schemas/index.js
+++ sanity/schemas/index.js
@@ -1,7 +1,9 @@
 import Interior from './interior-schema'
 import About from "./about-schema"
 import Product from "./product-schema"
+import Home from './home-schema'
+import FAQ from './faq'
 
-const schemas = [Product,Interior, About];
+const schemas = [Product,Interior, About, Home, FAQ];
 
 export default schemas;
