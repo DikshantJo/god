7147b0d87544bbf002d2f18ae47d0ad3937bc01b
DikshantJoshi <85272514+DikshantJo@users.noreply.github.com>
2023-10-02 16:43:28 +0530

    Update sanity-utils.js

--- sanity/sanity-utils.js
+++ sanity/sanity-utils.js
@@ -18,3 +18,39 @@ export async function getProjects() {
         }`
     );
 }
+
+
+export async function getProjectsInterior() {
+    const client = createClient({
+        projectId: 'nahm0f1b',
+        dataset: 'production',
+        apiVersion: '2023-10-01',
+    });
+
+    return client.fetch(
+        groq`*[_type == "Interior"] {
+            _id,
+            _createdAt,
+            name,
+            "image": image.asset->url, // Corrected the typo here
+            url,
+            content
+        }`
+    );
+}
+
+
+export async function getProjectFAQ() {
+    const client = createClient({
+        projectId: 'nahm0f1b',
+        dataset: 'production',
+        apiVersion: '2023-10-01',
+    });
+
+    return client.fetch(
+        groq`*[_type == "faq"] {
+          answer,
+          question
+        }`
+    );
+}
